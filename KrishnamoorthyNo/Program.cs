5555e720ed32ba58560b136ca519117a58a82401
Karthik Balakrishnan <karthik.balakrishnan@wonderbiz.in>
2024-07-10 11:50:08 +0530

    first commit

---
+++ KrishnamoorthyNo/Program.cs
@@ -0,0 +1,30 @@
+﻿public class KrishnamoorthyNo
+{
+    public static void Main(string[] args)
+    {
+        Console.Write("Enter a number: ");
+        string input = Console.ReadLine();
+        int sum = 0;
+
+        for (int i = 0; i < input.Length; i++)
+        {
+            int prod = 1;
+            int fact = 1;
+            for (int j = 1; j <= (input[i] - '0'); j++)
+            {
+                fact = j * prod;
+                prod = fact;
+            }
+            sum += prod;
+        }
+
+        if (sum == Convert.ToInt32(input))
+        {
+            Console.WriteLine("The entered number is a Krishnamoorthy number.");
+        }
+        else
+        {
+            Console.WriteLine("The entered number is not a Krishnamoorthy number.");
+        }
+    }
+}
---
+++ ValidAnagram/Program.cs
@@ -0,0 +1,46 @@
+﻿public class ValidAnagram
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Enter first string");
+        string string1 = Console.ReadLine();
+        Console.WriteLine("Enter second string");
+        string string2 = Console.ReadLine();
+        Console.WriteLine("Valid Anagram: {0}",Anagram(string1, string2));
+    }
+    public static bool Anagram(string s1, string s2)
+    {
+        Dictionary<char, int> map = new Dictionary<char, int>();
+        for (int i = 0; i < s1.Length; i++) 
+        {
+            if (map.ContainsKey(s1[i]))
+            {
+                map[s1[i]]++;
+            }
+            else
+            {
+                map.Add(s1[i], 1);
+            }
+        }
+        for (int i = 0; i < s2.Length; i++)
+        {
+            if (map.ContainsKey(s2[i]))
+            {
+                map[s2[i]]--;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        var keys = map.Keys;
+        foreach (char key in keys)
+        {
+            if (map[key] != 0)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}
